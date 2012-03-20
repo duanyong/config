7aae1c6650d27eaa530fdbdde558a786047d6234
duanyong <coderduan@gmail.com>
2012-03-21 00:43:01 +0800

    init.sh -> install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-sudo apt-get install -y vim meld fcitx-table-wubi git
 
-sudo apt-get build-dep -y autoconf libtool gobjc++ libxml2-dev zlib1g-dev
+#
+sudo apt-get install -y fcitx-table-wubi git mysql-server mysql-client php5 php5-cli php5-cgi php5-fpm php5-curl php5-mysql php5-memcache php-apc php-pear nginx
+
+#pman
+sudo pear channel-update doc.php.net
+sudo pear install doc.php.net/pman
+
+
+#vim 
+sudo apt-get install vim meld cscope exuberant-ctags
+
+
+  
+#sudo apt-get build-dep -y autoconf libtool gobjc++ libxml2-dev zlib1g-dev
 
 
