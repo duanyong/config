8458273f1294be11a6a8fa7a6689da848b38ddb4
duanyong <coderduan@gmail.com>
2012-03-29 00:45:40 +0800

    linux ssh

--- linux/install.sh
+++ linux/install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #
-sudo apt-get install -y fcitx-table-wubi git mysql-server mysql-client php5 php5-cli php5-cgi php5-fpm php5-curl php5-mysql php5-memcache php-apc php-pear nginx
+sudo apt-get install -y fcitx-table-wubi git mysql-server mysql-client php5 php5-cli php5-cgi php5-fpm php5-curl php5-mysql php5-memcache php-apc php-pear nginx ssh tree
 
 #pman
 sudo pear channel-update doc.php.net
 sudo pear install doc.php.net/pman
+pear install MDB2_Driver_mysql-1.5.0b3
 
 
 #vim 
